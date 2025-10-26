0422afaaa3daaa2b01ac4e1f011a45158aea07a5
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-10-26 14:11:20 +0300

    Implementing laravel like Routing with parameters

--- core/Router.php
+++ core/Router.php
@@ -2,16 +2,77 @@
 
 namespace Core;
 
-class Router {
+class Router
+{
   protected array $routes = [];
 
-  public function add(string $methid, string $uri, string $controller): void {}
+  public function add(string $method, string $uri, string $controller): void
+  {
+    $this->routes[] = [
+      'method' => $method,
+      'uri' => $uri,
+      'controller' => $controller
+    ];
+  }
 
-  public function dispatch(string $uri, string $method): string {}
+  public function notFound(): string
+  {
+    http_response_code(404);
+    return "404 Not Found";
+    exit;
+  }
 
-  protected function findRoute(string $uri, string $method): ?array {}
+  public function dispatch(string $uri, string $method): string
+  {
+    $route = $this->findRoute($uri, $method);
 
-  protected function matchRoute(string $routrUri, string $requestUri): ?array {}
+    if (!$route) {
+      return $this->notFound();
+    }
 
-  protected function callAction(string $controller, string $action, array $params): string {}
+    // PostController@index
+    // PostController@show
+    [$controller, $action] = explode('@', $route['controller']);
+    return $this->callAction($controller, $action, $route['params']);
+  }
+
+  protected function findRoute(string $uri, string $method): ?array
+  {
+    foreach ($this->routes as $route) {
+      $params = $this->matchRoute($route['uri'], $uri);
+      if ($params !== null && $route['method'] === $method) {
+        return [...$route, 'params' => $params];
+      }
+    }
+
+    return null;
+  }
+
+  protected function matchRoute(string $routeUri, string $requestUri): ?array
+  {
+    $routeSegments = explode('/', trim($routeUri, '/'));
+    $requestSegments = explode('/', trim($requestUri, '/'));
+
+    if (count($routeSegments) !== count($requestSegments)) {
+      return null;
+    }
+
+    $params = [];
+
+    foreach ($routeSegments as $index => $routeSegment) {
+      if (str_starts_with($routeSegment, '{') && str_ends_with($routeSegment, '}')) {
+        $params[trim($routeSegment, '{}')] = $requestSegments[$index];
+      } elseif ($routeSegment !== $requestSegments[$index]) {
+        return null;
+      }
+    }
+
+    return $params;
+  }
+
+  protected function callAction(string $controller, string $action, array $params): string
+  {
+    $conntrollerClass = "App\\Controller\\$controller";
+    return (new $conntrollerClass)->$action(...$params);
+  }
 }
--- public/index.php
+++ public/index.php
@@ -6,3 +6,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Core\Router;
 
 $router = new Router();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>OOP-PHP Blog</title>
+    <!-- The HTML link tag for the favicon -->
+    <link rel="icon" type="image/x-icon" href="/favicon">
+</head>
+<body>
+    <h1>Welcome to My Blog</h1>
+</body>
+</html>
