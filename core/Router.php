cb45dbfcb63ddc0d702782159bcb7749bebd0b47
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-10-26 10:11:09 +0300

    Init Blog website using OOP in php

---
+++ core/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core;
+
+class Router {
+  protected array $routes = [];
+
+  public function add(string $methid, string $uri, string $controller): void {}
+
+  public function dispatch(string $uri, string $method): string {}
+
+  protected function findRoute(string $uri, string $method): ?array {}
+
+  protected function matchRoute(string $routrUri, string $requestUri): ?array {}
+
+  protected function callAction(string $controller, string $action, array $params): string {}
+}
