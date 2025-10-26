60d55f693119d5ef671bc4ba49b85e6ccd06c3c9
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-10-26 14:45:02 +0300

    Defining Routes and creating cotrollers

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -6,16 +7,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Core\Router;
 
 $router = new Router();
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>OOP-PHP Blog</title>
-    <!-- The HTML link tag for the favicon -->
-    <link rel="icon" type="image/x-icon" href="/favicon">
-</head>
-<body>
-    <h1>Welcome to My Blog</h1>
-</body>
-</html>
+require_once __DIR__ . '/../routes.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_SERVER['REQUEST_METHOD'];
+echo $router->dispatch($uri, $method);
