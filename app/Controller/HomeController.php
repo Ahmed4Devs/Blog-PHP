0949fcf04e9c4da16f24e03c496bb6e3c6dad4c1
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-10-26 17:03:53 +0300

    Using Views and Adding main layout

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use Core\View;
+
 class HomeController
 {
   public function index()
   {
-    return 'Home';
+    return View::render(
+      template: 'home/index',
+      data: ['message' => 'Hello!'],
+      layout: 'layouts/main'
+    );
   }
 }
